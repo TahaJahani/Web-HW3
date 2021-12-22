dbff9320fed5a0891fc8b16e2537d524faa38ed1
TahaJahani <taha7900@gmail.com>
2021-12-23 01:32:38 +0330

    notes implemented

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { TextField, Card, Button, Container, Paper, Stack, Typography, Divider, ButtonGroup, colors } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+function Note(props) {
+    const {title, body, color} = props;
+    return(
+        <Card variant="outlined" sx={{backgroundColor: color, margin: 2, padding: 2}}>
+            <Typography variant='h6'>
+                {title}
+            </Typography>
+            <Divider />
+            <p>
+                {body}
+            </p>
+            <ButtonGroup variant='outlined' size='small'>
+                <Button color='primary'>
+                    <EditIcon />
+                </Button>
+                <Button color='error'>
+                    <DeleteIcon/>
+                </Button>
+            </ButtonGroup>
+        </Card>
+    )
+}
+
+export default Note;
